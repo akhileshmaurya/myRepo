627ab4d424fc7508edc32329b8f621198378cc79
akhileshmaurya <maurya_akhilesh@ymail.com>
2019-01-07 07:44:59 +0530

    Google Style and Formating

--- SpringData/src/main/java/com/example/data/es/ElasticSearchClient.java
+++ SpringData/src/main/java/com/example/data/es/ElasticSearchClient.java
@@ -4,7 +4,6 @@ package com.example.data.es;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.entity.ContentType;
@@ -13,28 +12,28 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 
 public class ElasticSearchClient {
-    private static ElasticSearchClient es = new ElasticSearchClient();
-    private RestClient restClient = null;
+  private static ElasticSearchClient es = new ElasticSearchClient();
+  private RestClient restClient = null;
 
-    public static ElasticSearchClient getElasticSearchClient() {
+  public static ElasticSearchClient getElasticSearchClient() {
 
-        return es;
-    }
+    return es;
+  }
 
-    private ElasticSearchClient() {
-        restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
-    }
+  private ElasticSearchClient() {
+    restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+  }
 
-    public void disconnect() throws IOException {
-        if (es != null && restClient != null)
-            restClient.close();
-    }
+  public void disconnect() throws IOException {
+    if (es != null && restClient != null)
+      restClient.close();
+  }
 
-    public Response runQuery(String jsonString) throws IOException {
-        Map<String, String> params = Collections.emptyMap();
-        HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
-        Response response = restClient.performRequest("PUT", "/posts/doc/1", params, entity);
-        return response;
-    }
+  public Response runQuery(String jsonString) throws IOException {
+    Map<String, String> params = Collections.emptyMap();
+    HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
+    Response response = restClient.performRequest("PUT", "/posts/doc/1", params, entity);
+    return response;
+  }
 
 }
